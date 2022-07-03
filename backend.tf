2d2ea27ee72f544dbf1816379b1228d4cb1e0ff3
chiedozie01 <104828833+chiedozie01@users.noreply.github.com>
2022-07-03 11:01:37 -0400

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstaten01528551RG"
-    storage_account_name = "tfstaten01528551sa"
-    container_name       = "tfstatefiles"
-    key                  = "h7m8PjhZqQ5wgCh5BvSsLkA4YIBdFtNBOyuGvLJQ3KdJdC3tVpK0daoVl+dflzHyD3nFzU8FqKgO+AStAb5c3w=="
-
-  }
-}
